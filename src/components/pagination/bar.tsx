7393b6c8cf16b49cf567fbfdb72dd145c68049ce
ayman.kaddioui <ayman.kaddioui@it.devcorp.fr>
2022-08-06 11:39:30 +0100

    feat: create PlayerCard & paginationBar

---
+++ src/components/pagination/bar.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+export default function PageBar({
+  pagesNumber,
+  currentPage,
+  onPageChange,
+}: {
+  pagesNumber: number;
+  currentPage: number;
+  onPageChange: Function;
+}) {
+  const pages = Array.apply(null, Array(5));
+  return (
+    <nav
+      className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+      aria-label="Pagination"
+    >
+      <button
+        disabled={currentPage === 1}
+        className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+      >
+        <span className="sr-only">Previous</span>
+        <svg
+          className="h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      {pages.map((_, index) => (
+        <button
+          key={index}
+          aria-current="page"
+          className={
+            index + 1 === currentPage
+              ? "z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+              : "bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+          }
+          onClick={() => onPageChange(index + 1)}
+        >
+          {index + 1}
+        </button>
+      ))}
+
+      <button
+        disabled={currentPage === pages.length}
+        className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+      >
+        <span className="sr-only">Next</span>
+        <svg
+          className="h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+    </nav>
+  );
+}
